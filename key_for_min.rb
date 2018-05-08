29a2800cc2e6b5ff8520877e597b802fc987abe3
Tony Ho <ho.tonym@gmail.com>
2018-05-08 21:41:01 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,10 @@
 
 def key_for_min_value(name_hash)
    if name_hash.length > 0
-
-   new_array= name_hash.collect do |x, y|
-     x<=>y
+     new_array = Array.new
+     new_array= name_hash.collect do |x, y|
+     x <=> y
+                end
      new_array[0]
    else
      nil
