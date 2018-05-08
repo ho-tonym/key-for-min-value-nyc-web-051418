a18dcfcfffff111a1973c551861d3c79ab0df5b6
Tony Ho <ho.tonym@gmail.com>
2018-05-08 21:51:36 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,11 @@
 
 def key_for_min_value(name_hash)
    if name_hash.length > 0
-     new_array = name_hash.max(name_hash.length)
-     new_array.reverse!
+
+     new_array = Array.new
+     new_array= name_hash.collect do |x, y|
+       y.max
+      end
      new_array[0]
    else
      nil
